2a973bacfc81c0d1ba3fda6d7e29aee84f38e5af
austin.rife <austin.rife@gmail.com>
2017-09-18 16:55:08 -0400

    refactored creeps to work with structures and energy harvesting

--- src/role.gatherer.js
+++ src/role.gatherer.js
@@ -3,24 +3,48 @@ var Role = require('role');
 
 class Gatherer extends Role {
     onTick() {
-        if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
+        if (canCarryEnergy()) {
+            harvestNearestSource();
         }
         else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.energy < structure.energyCapacity;
-                    }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
+            if (hasEnergy()) {
+                transferToNearestStructure();
             }
+        }
+    }
+    
+    canCarryEnergy() {
+        return self.carry.energy < self.carryCapacity;
+    }
+    
+    harvestNearestSource() {
+        var source = self.pos.findClosestByRange(FIND_SOURCES);
+        if (source != null) {
+            if (self.harvest(source) == ERR_NOT_IN_RANGE) {
+                self.moveTo(source);
+            }
+            
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    hasEnergy() {
+        return self.carry.energy > 0;
+    }
+    
+    transferToNearestStructure() {
+        var targets = creep.room.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                    structure.energy < structure.energyCapacity;
+            }
+        });
+
+        if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(target);
         }
     }
     
